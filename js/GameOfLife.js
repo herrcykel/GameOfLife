bc8643ff5aee40fb319a17738e1a8e82164d37d7
Olle Nilsson <nilssonn94@gmail.com>
2013-07-17 23:51:16 +0200

    Draw method implemented

--- js/GameOfLife.js
+++ js/GameOfLife.js
@@ -1,21 +1,20 @@
 var GameOfLife = (function (undefined) {
 
-    var CELL_MARGIN = 3;
-    var CELL_SIZE = 6;
+    var CELL_MARGIN = 1;
+    var CELL_SIZE = 8;
 
     var BACKGROUND_COLOR = "#FFFFFF";
     var CELL_COLOR = "#000000";
-    var GRID_COLOR = "#AAAAAA";
+    var GRID_COLOR = "#CCCCCC";
 
     function GameOfLife(canvas, gridSize, delay) {
 
         delay = delay || 500;
 
-        var interval;
+        var interval = null;
 
         var self = this;
         var cells = [];
-        var isRunning = false;
         var ctx = canvas.getContext("2d");
 
 
@@ -28,18 +27,43 @@ var GameOfLife = (function (undefined) {
         };
 
         var draw = function() {
-
+            ctx.fillStyle = GRID_COLOR;
+            ctx.fillRect(0, 0, canvas.width, canvas.height);
+            var curX = 0, curY = 0;
+            cells.forEach(function(col) {
+                col.forEach(function(cell) {
+                    if(cell.isAlive()) {
+                        ctx.fillStyle = CELL_COLOR;
+                    }
+                    else {
+                        ctx.fillStyle = BACKGROUND_COLOR;
+                    }
+                    ctx.fillRect(curX, curY, CELL_SIZE, CELL_SIZE);
+                    curX += CELL_SIZE + CELL_MARGIN;
+                });
+                curX = 0;
+                curY += CELL_SIZE + CELL_MARGIN;
+            });
         };
 
         var isOutOfBounds = function(pos) {
             return pos.row >= 0 && pos.col >= 0 && pos.row < canvas.height && pos.col < canvas.width;
         };
 
+        this.isRunning = function() {
+            return interval !== null;
+        }
+
         this.start = function() {
-            interval = setInterval(function() {
+            if(interval === null) {
                 update();
                 draw();
-            }, delay);
+
+                interval = setInterval(function() {
+                    update();
+                    draw();
+                }, delay);
+            }
         };
 
         this.stop = function() {
@@ -54,7 +78,7 @@ var GameOfLife = (function (undefined) {
                 var row = [];
                 for(var j = 0; j < gridSize;j++) {
                     var cell = new Cell(i, j);
-                    if(Math.random() > 0.25) { // Kill ~75%
+                    if(Math.random() > 0.1) { // Kill ~90%
                         cell.kill();
                     }
                     row.push(cell);
